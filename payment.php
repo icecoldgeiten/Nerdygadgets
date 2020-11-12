e9bedcffbae57503edb2860db39a483f279d4cb4
Your Name <icecold2000@hotmail.nl>
2020-11-12 13:10:50 +0100

    Add payment-page

---
+++ payment.php
@@ -0,0 +1,12 @@
+<?php
+include __DIR__ . "/header.php";
+?>
+<div class="IndexStyle">
+    <div class="col-11">
+        //hier gaan we componenten inladen
+    </div>
+</div>
+<?php
+include __DIR__ . "/footer.php";
+?>
+
