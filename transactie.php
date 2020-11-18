5f39af69fced9978c535899c1dd3d5e6eefa833d
NGoossen01 <NGoossen01@users.noreply.github.com>
2020-11-18 10:50:41 +0100

    Update transactie.php

--- transactie.php
+++ transactie.php
@@ -1,6 +1,6 @@
 <?php
-include "../connect.php";
-include __DIR__ . "/../header.php";
+include __DIR__ . "/connect.php";
+include __DIR__ . "/header.php";
 if ($_GET["betaling"] === "Betaling gelukt!") {
     echo("<h1>De betaling is gelukt!</h1><br>");
     echo("U krijgt nu een orderbevestiging en een factuur via de mail.<br>");
@@ -13,5 +13,5 @@ if ($_GET["betaling"] === "Betaling mislukt!") {
 <br>
 <a href='index.php'>Ga hier terug naar de homepage...</a>
 <?php
-include __DIR__ . "/../footer.php";
+include __DIR__ . "/footer.php";
 ?>
