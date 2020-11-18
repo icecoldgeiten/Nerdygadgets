8968f39721a1290d680499297a6993918fb72c31
NGoossen01 <NGoossen01@users.noreply.github.com>
2020-11-18 10:47:48 +0100

    Update transactie.php

--- pay/transactie.php
+++ pay/transactie.php
@@ -1,24 +1,17 @@
 <?php
 include "../connect.php";
+include __DIR__ . "/../header.php";
+if ($_GET["betaling"] === "Betaling gelukt!") {
+    echo("<h1>De betaling is gelukt!</h1><br>");
+    echo("U krijgt nu een orderbevestiging en een factuur via de mail.<br>");
+}
+if ($_GET["betaling"] === "Betaling mislukt!") {
+    echo("<h1>De betaling is niet gelukt!</h1><br>");
+    echo("Je kan nog op de terugknop drukken om de betaling alsnog te voltooien.");
+}
 ?>
-<!DOCTYPE html>
-<html>
-    <head>
-        <meta charset="utf-8">
-    </head>
-    <body>
-        <?php
-        include __DIR__ . "/../header.php";
-        if ($_GET["betaling"] === "Betaling gelukt!") {
-            echo ("<h1>De betaling is gelukt!</h1><br>");
-            echo ("U krijgt nu een orderbevestiging en een factuur via de mail.<br>");
-        }
-        if ($_GET["betaling"] === "Betaling mislukt!") {
-            echo ("<h1>De betaling is niet gelukt!</h1><br>");
-            echo ("Je kan nog op de terugknop drukken om de betaling alsnog te voltooien.");
-        }
-        ?>
-        <br>
-        <a href='index.php'>Ga hier terug naar de homepage...</a>
-    </body>
-</html>
+<br>
+<a href='index.php'>Ga hier terug naar de homepage...</a>
+<?php
+include __DIR__ . "/../footer.php";
+?>
