5e31941a48673a4ab4bd8af88712856b44ddde2e
NGoossen01 <NGoossen01@users.noreply.github.com>
2020-11-18 09:53:37 +0100

    Fix legal issue
    Deleted textbox asking creditcard information

--- pay/payment-providers/creditcard.php
+++ pay/payment-providers/creditcard.php
@@ -5,10 +5,6 @@
         <option value='Mastercard'>Mastercard</option>
         <option value='American Express'>American Express</option>
     </select><br>
-    Creditcard-nummer:  <input type='text' name='creditcard-nummer' placeholder='XXXX-XXXX-XXXX-XXXX'><br>
-    Geldig tot:         <input type='text' name='creditcard-maand' placeholder='MM' size='1'> / <input type='text' name='creditcard-jaar' placeholder='YY' size='1'><br>
-    Naam op kaart:      <input type='text' name='creditcard-naam' placeholder='J.J. Janssen'><br>
-    CVC-code:           <input type='text' name='creditcard-cvc' placeholder='XXX' size='2'><br>
     <br>
     <input type='submit' value='Bestellen'>
 </form>
