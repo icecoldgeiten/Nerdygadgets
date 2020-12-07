2d1f22867aa49b03445f7e67c2b8bf1bbe498635
Your Name <icecold2000@hotmail.nl>
2020-12-07 13:47:49 +0100

    Fix style

--- payment.php
+++ payment.php
@@ -87,7 +87,6 @@ include __DIR__ . "/orderfunctions.php";
     </div>
 </div>
 <div class="row">
-=======
     <!--        <div class="col-md-6 col-xs-12">-->
     <!--            <div class="row">-->
     <!--                <div class="col-md-12 pr-5">-->
