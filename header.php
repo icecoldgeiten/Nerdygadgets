62d1a945747136c7036e49a474aadf5267f4e9bf
Your Name <icecold2000@hotmail.nl>
2020-12-07 14:48:57 +0100

    Add minor fixes

--- header.php
+++ header.php
@@ -60,7 +60,9 @@ $HeaderStockGroups = mysqli_stmt_get_result($Statement);
             aria-controls="navbarTogglerDemo03" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
     </button>
-    <a class="navbar-brand" href="./"><div id="LogoImage"></div></a>
+    <a class="navbar-brand" href="./">
+        <div id="LogoImage"></div>
+    </a>
 
     <div class="collapse navbar-collapse" id="navbarTogglerDemo03">
         <ul class="navbar-nav mr-auto mt-2 mt-lg-0">
@@ -77,17 +79,17 @@ $HeaderStockGroups = mysqli_stmt_get_result($Statement);
         </ul>
         <ul class="navbar-nav">
             <li class="nav-item float-right">
-                <a href="cart.php" class="HrefDecoration mr-3"><i class="fas fa-shopping-cart mr-2" style="color:#676EFF;"></i>Winkelmand</a>
+                <a href="cart.php" class="HrefDecoration mr-3"><i class="fas fa-shopping-cart mr-2"
+                                                                  style="color:#676EFF;"></i>Winkelmand</a>
             </li>
             <li class="nav-item float-right">
-                <a href="browse.php" class="HrefDecoration"><i class="fas fa-search mr-2" style="color:#676EFF;"></i> Zoeken</a>
-
+                <a href="browse.php" class="HrefDecoration"><i class="fas fa-search mr-2" style="color:#676EFF;"></i></a>
             </li>
-          <li>
-              <a href="login.php" class="HrefDecoration"><i class="fas fa-sign-in-alt"></i>Inloggen</a>
-          </li>
+            <li class="nav-item float-right">
+                <a href="login.php" class="HrefDecoration"><i class="fas fa-sign-in-alt mr-2 ml-2" style="color:#676EFF;"></i>Inloggen</a>
+            </li>
         </ul>
     </div>
 </nav>
 <div class="container mb-5" id="Content">
-        <div id="SubContent">
+    <div id="SubContent">
