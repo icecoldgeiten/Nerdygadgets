e8a8e2796198ea9ca7d41bc196b79bb0fed01822
babelsk <74234300+babelsk@users.noreply.github.com>
2020-12-08 09:20:20 +0100

    Updated Database account

--- connect.php
+++ connect.php
@@ -1,7 +1,7 @@
 <?php
 mysqli_report(MYSQLI_REPORT_ERROR | MYSQLI_REPORT_STRICT);
 try {
-    $Connection = mysqli_connect("localhost", "viewaccount", "IkWilKijken", "nerdygadgets");
+    $Connection = mysqli_connect("localhost", "root", "SuperGeheimWachtwoord", "nerdygadgets");
     mysqli_set_charset($Connection, 'latin1');
     $DatabaseAvailable = true;
 } catch (mysqli_sql_exception $e) {
