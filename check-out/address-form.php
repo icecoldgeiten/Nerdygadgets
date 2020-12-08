48b7bbe62f01111e1ebcb3d3c88fe2166ac1950a
BB1-23 <74234508+BB1-23@users.noreply.github.com>
2020-12-08 15:28:23 +0100

    Update address-form.php

--- check-out/address-form.php
+++ check-out/address-form.php
@@ -1,7 +1,7 @@
 <?php
 include "delivery-form.php";
 ?>
-<form method="get" action="">
+<form method="get" action="<?php print(htmlspecialchars($_SERVER["PHP_SELF"]));?>">
     <input type="checkbox" name="postal">Bezorgadres is niet hetzelfde als factuuradres<br>
     <br>
     <input type="submit" value="Ga door...">
