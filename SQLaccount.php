83907b1103f6e72dc47ef8556474828fbac50fcf
babelsk <74234300+babelsk@users.noreply.github.com>
2020-12-09 10:27:10 +0100

    fix passwords

--- SQLaccount.php
+++ SQLaccount.php
@@ -1,7 +1,7 @@
 <?php
 mysqli_report(MYSQLI_REPORT_ERROR | MYSQLI_REPORT_STRICT);
 try {
-    $Connection = mysqli_connect("localhost", "Customer", "IkWilKopen", "nerdygadgets");
+    $Connection = mysqli_connect("localhost", "Customer", "ETYfo5%DyCuBD0@wcR0", "nerdygadgets");
     mysqli_set_charset($Connection, 'latin1');
     $DatabaseAvailable = true;
 } catch (mysqli_sql_exception $e) {
--- connect.php
+++ connect.php
@@ -1,7 +1,7 @@
 <?php
 mysqli_report(MYSQLI_REPORT_ERROR | MYSQLI_REPORT_STRICT);
 try {
-    $Connection = mysqli_connect("localhost", "viewaccount", "IkWilKijken", "nerdygadgets");
+    $Connection = mysqli_connect("localhost", "viewaccount", "c!05*6!pXbWlW8a5&Py", "nerdygadgets");
     mysqli_set_charset($Connection, 'latin1');
     $DatabaseAvailable = true;
 } catch (mysqli_sql_exception $e) {
